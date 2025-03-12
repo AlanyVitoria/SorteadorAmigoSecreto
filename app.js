3f44d421b5f6e7e1ba9c4a5c885a8c76745394eb
Alany Vitória <alanyvitoria03@gmail.com>
2025-03-12 13:25:07 -0300

    sortear amigo

--- app.js
+++ app.js
@@ -6,11 +6,30 @@ let listaDeAmigos=[];
 function adicionarAmigo() {
     if (nome === "") {
         alert("Por favor, adicione um nome!");
-    } else {
-        listaDeAmigos.push(nome);
-        atualizarLista();
-        inputAmigos.value = "";
+        return;
+    }else{
+    listaDeAmigos.push(nome);
     }
+    listaHtml.innerHTML = "";
+
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = listaDeAmigos[i];
+        listaHtml.appendChild(li);
+    }
+
+    inputAmigos.value = "";
+}
+botaoAdicionar.onclick = adicionarAmigo;
+
+function sortearAmigo() {
+    if (listaDeAmigos.length === 0) {
+        alert("Não há amigos disponíveis para sortear.");
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    let amigoSorteado = listaDeAmigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = "O amigo sorteado é: " + amigoSorteado;
 }
 
-botaoAdicionar.onclick = adicionarAmigo
